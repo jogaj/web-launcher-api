aab9afd70da1b13de449eb50979771d3d5a6cd6e
Jonathan <jogaj_26@hotmail.com>
2025-12-19 04:04:36 +0000

    Update connection

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,30 +4,15 @@ import pg from 'pg';
 
 dotenv.config();
 
-// const sequelize = new Sequelize(
-// 	process.env.DB_NAME ?? 'db',
-// 	process.env.DB_USER ?? 'user',
-// 	process.env.DB_PASSWORD ?? 'password',
-// 	{
-// 		host: process.env.DB_HOST ?? 'host',
-// 		dialect: 'postgres',
-// 		port: parseInt(process.env.DB_HOST_PORT ?? '5432'),
-// 	},
-// );
-
-// const sequelize = new Sequelize("postgres://postgres:Zt7P2QxHUfTL@db.jxhtadplgvivduoigjuz.supabase.co:5432/postgres"
-// );
-
-// const sequelize = new Sequelize('postgresql://postgres.jxhtadplgvivduoigjuz:Zt7P2QxHUfTL@aws-1-us-east-1.pooler.supabase.com:5432/postgres');
-
 const sequelize = new Sequelize(
-	'postgres',
-	'postgres.jxhtadplgvivduoigjuz',
-	'Zt7P2QxHUfTL',
+	process.env.DB_NAME ?? 'db',
+	process.env.DB_USER ?? 'user',
+	process.env.DB_PASSWORD ?? 'password',
 	{
-		host: 'aws-1-us-east-1.pooler.supabase.com',
+		host: process.env.DB_HOST ?? 'host',
 		dialect: 'postgres',
-		dialectModule: pg
+		dialectModule: pg,
+		port: parseInt(process.env.DB_HOST_PORT ?? '5432'),
 	},
 );
 
